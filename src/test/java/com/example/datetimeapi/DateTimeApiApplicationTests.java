1c9cd60e2da977dfa53346eeb51d0de8bcbed5f0
aoezalti <ayse.oezaltin@outlook.com>
2022-06-22 19:47:32 +0200

    Initial commit

---
+++ src/test/java/com/example/datetimeapi/DateTimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datetimeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateTimeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
