ddfd6f3a94cfd3dfd167fa542580fedba72b90fd
aoezalti <ayse.oezaltin@outlook.com>
2022-06-22 20:49:20 +0200

    implemented /api/now API

--- src/main/java/com/example/datetimeapi/DateTime.java
+++ src/main/java/com/example/datetimeapi/DateTime.java
@@ -1,10 +1,14 @@
 package com.example.datetimeapi;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 
 public class DateTime {
 
     public LocalDate getDate () {
         return LocalDate.now();
+    }
+    public LocalTime getTime () {
+        return LocalTime.now();
     }
 }
